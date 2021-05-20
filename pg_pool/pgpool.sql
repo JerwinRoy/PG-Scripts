2dfc163bfbdac208e3f829f503398b566183636e
Jerwin Roy <JerwinRoy@users.noreply.github.com>
2021-05-20 11:20:44 +0530

    Create pgpool.sql

---
+++ pg_pool/pgpool.sql
@@ -0,0 +1,26 @@
+-- CHECK STATUS/START/STOP
+systemctl status pgpool-II-11
+systemctl restart pgpool-II-11
+systemctl start pgpool-II-11
+systemctl stop pgpool-II-11
+
+-- GET INFO ABOUT EACH NODES
+pcp_node_info -n 0 -U user -h localhost
+pcp_node_info -n 1 -U user -h localhost
+
+-- ATTACH A NODE
+pcp_attach_node -n 0 -U user 
+pcp_attach_node -n 1 -U user
+
+-- DETACH A NODE
+pcp_detach_node -n 0 -U user
+pcp_detach_node -n 1 -U user
+
+-- CHECK CONNECTIVITY FROM POSTGRES USING HOST
+psql -d postgres -h hostname -U user
+
+-- LOGIN TO PGPOOL
+psql -U user -d postgres -h pgpool_host
+
+-- CHECK NODES STATUS AFTER LOGGINH IN 
+show pool_nodes;

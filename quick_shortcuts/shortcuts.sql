c4cb5f55b61d295fd200e3c20e39c060f20518fd
Jerwin Roy <JerwinRoy@users.noreply.github.com>
2021-05-20 10:04:52 +0530

    Create shortcuts.sql

---
+++ quick_shortcuts/shortcuts.sql
@@ -0,0 +1,77 @@
+\dd object-name
+Display comments for object-name
+
+\db
+List all tablespaces
+
+\dn
+List all schemas
+
+\d_\dt
+List all tables
+
+\di
+List all indexes
+
+\ds
+List all sequences
+
+\dv
+List all views
+
+\dS
+List all PostgreSQL-defined tables
+
+\d table-name
+Show table definition
+
+\d index-name
+Show index definition
+
+\d view-name
+Show view definition
+
+\d sequence-name
+Show sequence definition
+
+\dp
+List all privileges
+
+\dl
+List all large objects
+
+\da
+List all aggregates
+
+\df
+List all functions
+
+\dc
+List all conversions
+
+\dC
+List all casts
+
+\df function-name
+List all functions with given name
+
+\do
+List all operators
+
+\do operator-name
+List all operators with given name
+
+\dT
+List all types
+
+\dD
+List all domains
+
+\dg
+List all groups
+
+\du
+List all users
+
+\l
+List all databases in this cluster

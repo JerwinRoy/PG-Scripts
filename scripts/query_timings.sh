7db91120b09a544319f603cef47c75e6b27f36da
Jerwin Roy <JerwinRoy@users.noreply.github.com>
2021-05-26 22:45:52 +0530

    Update query_timings.sh

--- scripts/query_timings.sh
+++ scripts/query_timings.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 #ACTIVE QUERY BY TIMINGS 
 
-pg_dir=/usr/pgsql-13/bin
-host=localhost
-user=jerwin
-database=postgres
-port=5432
+PG_PATH=/usr/pgsql-13/bin
+PG_HOST=localhost
+PG_USER=jerwin
+PG_DB=postgres
+PG_PORT=5432
 #query=select pid as PID,datname as DATABASE,usename as USER,client_addr as HOST,now() - query_start as DURATION,state as STATE,query as QUERY from pg_stat_activity where state='active' order by duration desc;
 #out_file=/var/log/pg_query_timings.log
 
-pqt=$($pg_dir/psql -h $host -U $user -d $database -p $port --no-psqlrc -q -t -A -c "select pid as PID,datname as DATABASE,usename as USER,client_addr as HOST,now() - query_start as DURATION,state as STATE,query as QUERY from pg_stat_activity where state='active' order by duration desc")
+pqt=$($PG_PATH/psql -h $PG_HOST -U $PG_USER -d $PG_DB -p $PG_PORT --no-psqlrc -q -t -A -c "select pid as PID,datname as DATABASE,usename as USER,client_addr as HOST,now() - query_start as DURATION,state as STATE,query as QUERY from pg_stat_activity where state='active' order by duration desc")
 echo "--------------------------------------"
-echo "      POSTGRES QUERY TIMINGS          "
+echo "  POSTGRES QUERY BY TIMINGS           "
 echo "--------------------------------------"
 `printf "\n"`
 

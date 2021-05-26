806db8b7d9674c2b7a06ce49663a02079a7bbbc3
Jerwin Roy <JerwinRoy@users.noreply.github.com>
2021-05-26 20:50:34 +0530

    Create query_timings.sh

---
+++ scripts/query_timings.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#ACTIVE QUERY BY TIMINGS 
+
+pg_dir=/usr/pgsql-13/bin
+host=localhost
+user=jerwin
+database=postgres
+port=5432
+#query=select pid as PID,datname as DATABASE,usename as USER,client_addr as HOST,now() - query_start as DURATION,state as STATE,query as QUERY from pg_stat_activity where state='active' order by duration desc;
+out_file=/var/log/pg_query_timings.log
+
+$pg_dir/psql -h $host -U $user -d $database -p $port --no-psqlrc -q -t -A -c "select pid as PID,datname as DATABASE,usename as USER,client_addr as HOST,now() - query_start as DURATION,state as STATE,query as QUERY from pg_stat_activity where state='active' order by duration desc" >> $out_file

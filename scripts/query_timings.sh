69d599f09717d40fc56c109282040c21ffdc3b23
Jerwin Roy <JerwinRoy@users.noreply.github.com>
2021-05-26 22:30:45 +0530

    Update query_timings.sh

--- scripts/query_timings.sh
+++ scripts/query_timings.sh
@@ -7,6 +7,12 @@ user=jerwin
 database=postgres
 port=5432
 #query=select pid as PID,datname as DATABASE,usename as USER,client_addr as HOST,now() - query_start as DURATION,state as STATE,query as QUERY from pg_stat_activity where state='active' order by duration desc;
-out_file=/var/log/pg_query_timings.log
+#out_file=/var/log/pg_query_timings.log
 
-$pg_dir/psql -h $host -U $user -d $database -p $port --no-psqlrc -q -t -A -c "select pid as PID,datname as DATABASE,usename as USER,client_addr as HOST,now() - query_start as DURATION,state as STATE,query as QUERY from pg_stat_activity where state='active' order by duration desc" >> $out_file
+pqt=$($pg_dir/psql -h $host -U $user -d $database -p $port --no-psqlrc -q -t -A -c "select pid as PID,datname as DATABASE,usename as USER,client_addr as HOST,now() - query_start as DURATION,state as STATE,query as QUERY from pg_stat_activity where state='active' order by duration desc")
+echo "--------------------------------------"
+echo "      POSTGRES QUERY TIMINGS          "
+echo "--------------------------------------"
+`printf "\n"`
+
+echo $pqt
